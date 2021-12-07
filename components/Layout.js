6d91354db0ab5c4df6c32272854afb3f106111a8
yuta000111 <yuuta0114@gmail.com>
2021-12-07 09:10:30 +0900

    AuthComponent作成

---
+++ components/Layout.js
@@ -0,0 +1,18 @@
+import Head from 'next/head';
+
+export default function Layout({
+  children,
+  title = 'Default title',
+}) {
+  return (
+    <div className="flex justify-center items-center flex-col min-h-screen text-gray-600 text-sm font-mono bg-gray-800">
+      <Head>
+        <title>{title}</title>
+      </Head>
+      <main className="flex flex-1 justify-center items-center w-screen flex-col">
+        {children}
+      </main>
+      <footer>@2021 12</footer>
+    </div>
+  );
+}
---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Auth from '../components/Auth';
+import Layout from '../components/Layout';
+
+export default function Home() {
+  return (
+    <Layout title="login">
+      <Auth />
+    </Layout>
+  );
+}
